b64bce30ee7deab77c507c1ae042b11cf619f4cb
DevAlexFR <alexsandro77flores@gmail.com>
2024-12-12 23:09:10 -0300

    .

--- project/assets/js/script.js
+++ project/assets/js/script.js
@@ -1,16 +1,41 @@
 console.log('Site loaded successfully.');
 
-// Função para abrir o modal
+// Função para abrir o modal e carregar o vídeo
 function openModal(modalId) {
     var modal = document.getElementById(modalId);
-    modal.style.display = "flex";
+    var iframe = modal.querySelector('iframe');
+
+    switch(modalId) {
+        case 'modal_Baba':
+            iframe.src = 'https://www.youtube.com/embed/Cz-MvM3Who0';
+            break;
+        case 'modal_Castlevania':
+            iframe.src = 'https://www.youtube.com/embed/-5B-15TyGTk';
+            break;
+        case 'modal_CVD':
+            iframe.src = 'https://www.youtube.com/embed/jwiCmA5vX-A';
+            break;
+        case 'modal_Casa':
+            iframe.src = 'https://www.youtube.com/embed/iS5xXr-GOnM';
+            break;
+        case 'modal_Justiceiro':
+            iframe.src = 'https://www.youtube.com/embed/STuXgXcOVPE';
+            break;
+        case 'modal_Mogli':
+            iframe.src = 'https://www.youtube.com/embed/fXE1Vv0-PJQ';
+            break;
+    }
+
+    modal.style.display = "block";
 }
 
-// Fecha o modal ao clicar no "x"
+// Função para fechar o modal e parar o vídeo
 var closeButtons = document.getElementsByClassName("closeBtn");
 for (var i = 0; i < closeButtons.length; i++) {
     closeButtons[i].onclick = function() {
         var modal = this.closest('.modal');
+        var iframe = modal.querySelector('iframe');
+        iframe.src = ''; // Limpa o src para parar o vídeo
         modal.style.display = "none";
     }
 }
@@ -18,6 +43,8 @@ for (var i = 0; i < closeButtons.length; i++) {
 // Fecha o modal ao clicar fora dele
 window.onclick = function(event) {
     if (event.target.classList.contains('modal')) {
+        var iframe = event.target.querySelector('iframe');
+        iframe.src = ''; // Limpa o src para parar o vídeo
         event.target.style.display = "none";
     }
 }
